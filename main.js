65673ba96c2d108cfb38a0ec949c2feb8cd6f9b8
AndrewJR <reginaldoandrew07@gmail.com>
2024-02-26 19:05:51 -0300

    Finalização da criação do Card dos produtos
    A finalização da construção dos cards dos produtos finaliza a seção.

---
+++ main.js
@@ -0,0 +1,10 @@
+// const duplicarElemento = () => {
+//   let elementoOriginal = document.getElementById("elemOri");
+
+//   let elementoDuplicado = elementoOriginal.cloneNode(true);
+
+//   let testando = document.getElementById("elemDupl");
+
+//   testando.appendChild(elementoDuplicado);
+// };
+//
