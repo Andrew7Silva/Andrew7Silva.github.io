9caa4705a1d465605a68365aee0aaa42bdeaf3a4
AndrewJR <reginaldoandrew07@gmail.com>
2024-02-28 14:49:13 -0300

    HomePage concluída.
    Criação do Blog concluído.
    + algumas correções.
    Falta apenas a revisão.

--- main.js
+++ main.js
@@ -1,10 +1,86 @@
-// const duplicarElemento = () => {
-//   let elementoOriginal = document.getElementById("elemOri");
+// Duplicar Card Item
+let frameOriginal = document.getElementById("frameItem");
 
-//   let elementoDuplicado = elementoOriginal.cloneNode(true);
+let frameArray = [
+  document.getElementById("frameDupl1"),
+  document.getElementById("frameDupl2"),
+  document.getElementById("frameDupl3"),
+  document.getElementById("frameDupl4"),
+  document.getElementById("frameDupl5"),
+  document.getElementById("frameDupl6"),
+];
 
-//   let testando = document.getElementById("elemDupl");
+let framesDuplicados = [];
 
-//   testando.appendChild(elementoDuplicado);
-// };
-//
+const duplicarFrame = () => {
+  for (i = 0; i < frameArray.length; i++) {
+    let clone = frameOriginal.cloneNode(true);
+    frameArray[i].appendChild(clone);
+    framesDuplicados.push(clone);
+  }
+  return framesDuplicados;
+};
+
+// Duplicar Grupo de Cartas
+const groupOriginal = document.getElementById("groupCards");
+const groupElement = document.getElementById("groupDupl");
+
+const duplicarGroup = () => {
+  const groupDuplicado = groupOriginal.cloneNode(true);
+  return groupElement.appendChild(groupDuplicado);
+};
+
+// Duplicar Tabela de Produtos
+const tabProductOriginal = document.getElementById("tabProduct");
+const tabProductElement = document.getElementById("tabProductDupl");
+
+const duplicarTabProduct = () => {
+  const tabProductDuplicado = tabProductOriginal.cloneNode(true);
+  return tabProductElement.appendChild(tabProductDuplicado);
+};
+
+// Duplicar flexRow
+const flexRowOriginal = document.getElementById("flexRow");
+const flexRowArray = [
+  document.getElementById("flexRowDupl"),
+  document.getElementById("flexRowDupl1"),
+  document.getElementById("flexRowDupl2"),
+];
+let flexRowDuplicados = [];
+
+const duplicarFlexRow = () => {
+  for (i = 0; i < flexRowArray.length; i++) {
+    let clone = flexRowOriginal.cloneNode(true);
+    flexRowArray[i].appendChild(clone);
+    flexRowDuplicados.push(clone);
+  }
+  return flexRowDuplicados;
+};
+
+// Duplicar Texto do blog
+const textBlogOriginal = document.getElementById("blogText");
+
+const textBlogArray = [
+  document.getElementById("blogTextDupl"),
+  document.getElementById("blogTextDupl1"),
+];
+
+let textBlogDuplicados = [];
+
+const duplicarTextBlog = () => {
+  for (i = 0; i < textBlogArray.length; i++) {
+    let clone = textBlogOriginal.cloneNode(true);
+    textBlogArray[i].appendChild(clone);
+    textBlogDuplicados.push(clone);
+  }
+  return textBlogDuplicados;
+};
+
+// Chamada das Funções
+window.onload = () => {
+  duplicarFrame();
+  duplicarGroup();
+  duplicarTabProduct();
+  duplicarFlexRow();
+  duplicarTextBlog();
+};
